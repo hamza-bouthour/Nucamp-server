c39fab2a48e3c9314e2f27ec3c84d908916bad89
Hamza Bouthour <bouthour.h@gmail.com>
2021-04-22 09:25:24 -0700

    Passport and Token-Based Authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const passport = require('passport');
 const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,8 +16,7 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
 const mongoose = require('mongoose');
-
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -53,20 +53,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-      const err = new Error('You are not authenticated!');                    
-      err.status = 401;
-      return next(err);
-  } else {
-      return next();
-  }
-}
-
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
