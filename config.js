4e4126501280bf0f9ad716c0be67de134ff61382
Hamza Bouthour <bouthour.h@gmail.com>
2021-04-28 20:09:07 -0700

    facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '314317056798816',
+        clientSecret: 'c8d555d4fc1d20d893b5df655e06dd54'
+    }
 }
